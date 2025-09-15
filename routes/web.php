81282654b6e5002cf0fdcdc9de3bab39c3d80482
Andrey Poddubniy <pdandreyv@gmail.com>
2025-09-15 20:17:17 +0300

    ф

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Маршрут для авторизации - перенаправляем на rostpack
+Route::get('/login', function () {
+    return redirect()->route('rostpack.index');
+})->name('login');
